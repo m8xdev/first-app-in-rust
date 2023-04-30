42d2a9e0db2ed957381d3f8ac95b035cc35f09d9
Igor Golovko <m8x@localhost.localdomain>
2023-04-30 14:20:08 +0300

    Commit

---
+++ main.rs
@@ -0,0 +1,10 @@
+/*
+    * First Application in Rust
+    * Created by Group Reflection, Inc.
+    * 2023
+    * Licensed under the AGPLv3 License
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
